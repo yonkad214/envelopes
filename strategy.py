5b8a071d7b2ef9a56389d07bb56fd1cfdc65ca92
yonkad214 <yonkad214@gmail.com>
2020-09-30 10:28:58 +0300

    adding basestrategy and More_then_N_percent_group_strategy

---
+++ strategy.py
@@ -0,0 +1,40 @@
+from envelope import Envelope
+
+class BaseStrategy():
+
+    def __init__(self,envelopes):
+        self._envelopes = envelopes
+
+    def play(self):
+
+
+        for i in range(len(self._envelopes)):
+            print(self._envelopes[i].money)
+            stop = input("Press Y to stop")
+            if stop == "Y":
+                return self._envelopes[i]
+            if i == 100:
+                return self._envelopes[i]
+
+
+        
+
+class More_then_N_percent_group_strategy(BaseStrategy):
+    def __init__(self, envelopes, precent = 0.25):   
+        BaseStrategy.__init__(self,envelopes)  
+        self._precent = precent
+
+    def play(self):
+        max = 0
+        for i in range(self._precent*100):
+            if(self._envelopes[i].money > max):
+                max = self._envelopes[i].money
+        for i in range(self._precent*100, len(self._envelopes)):
+            if(self._envelopes[i].money > max):
+                return self._envelopes[i]
+        return self._envelopes[len(self._envelopes)] 
+
+
+    @property
+    def precent(self):
+        return self._precent
