8d91cf27199b31782f9050c8aca5079100966afd
גיא אביב <guyaviv180@gmail.com>
2020-10-10 15:57:45 +0300

    added n_max

--- strategy.py
+++ strategy.py
@@ -16,9 +16,6 @@ class BaseStrategy():
             if i == 100:
                 return self._envelopes[i]
 
-
-        
-
 class More_then_N_percent_group_strategy(BaseStrategy):
     def __init__(self, envelopes, precent = 0.25):   
         BaseStrategy.__init__(self,envelopes)  
@@ -38,3 +35,19 @@ class More_then_N_percent_group_strategy(BaseStrategy):
     @property
     def precent(self):
         return self._precent
+
+class N_max_strategy(BaseStrategy):
+    def __init__(self, envelopes, N):
+        BaseStrategy.__init__(self, envelopes)
+        self.N = N
+
+    def play(self, N):
+        maxAmount = self._envelopes[0]
+        counter = 1;
+        for i in range (len(self._envelopes)):
+            if(self._envelopes[i] > max):
+                maxAmount = self._envelopes[i]
+                counter += 1
+            if(counter == N):
+                break
+        return maxAmount
